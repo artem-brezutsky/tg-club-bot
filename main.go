8578cefa86d89e089a26cb6dd5a4a7e175cd4ae5
artem-brezutsky <artem-brezutsky>
2023-02-15 01:17:19 +0200

    upload photo

--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 const StatusNew = 2
 const StatusAccepted = 3
 const StatusDeclined = 4
 const StatusWaiting = 5
+
+// todo Сделать проверку того что отправляют в ответе, что бы текст был текстом, не стикер или эмодзи!!! Иначе возвращать на шаг назад
 
 // Request Сущность пользователя
 type Request struct {
@@ -52,9 +55,23 @@ func main() {
 		panic("failed to connect database")
 	}
 
-	answer1 := ""
-	answer2 := ""
-	answer3 := ""
+	// Ответы на вопросы
+	var (
+		answer1 = ""
+		answer2 = ""
+		answer3 = ""
+	)
+
+	var doneButton = tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Готово👌"),
+		),
+	)
+
+	// Массив ИД файлов для отправки
+	var answerFileIds []string
+	var isDocumentFiles = false
+	var isPhotoFiles = false
 
 	for update := range updates {
 		if update.Message != nil { // If we got a message
@@ -67,37 +84,67 @@ func main() {
 			var userRequest Request
 
 			// @todo тестируем
-			if update.Message.From.ID == OwnerAcc {
-
+			if update.Message.From.ID == 123 {
+				//msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+				//
+				//switch update.Message.Text {
+				//case "open":
+				//	msg.ReplyMarkup = but
+				//case "close":
+				//	msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+				//}
+				//
+				//if _, err := bot.Send(msg); err != nil {
+				//	log.Panic(err)
+				//}
+				//
+				//continue
 				// Если это фото
 				if update.Message.Photo != nil {
-					//file, _ := bot.GetFile(tgbotapi.FileConfig{FileID: update.Message.Photo[1].FileID})
-					msg := tgbotapi.NewPhoto(OwnerAcc, tgbotapi.FileID(update.Message.Photo[0].FileID))
-
+					//msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+					answerFileIds = append(answerFileIds, update.Message.Photo[1].FileID)
+					msg.ReplyMarkup = doneButton
 					bot.Send(msg)
+				} else if update.Message.Document != nil &&
+					strings.Contains(update.Message.Document.MimeType, "image") {
+					//msg := tgbotapi.NewDocument(OwnerAcc, tgbotapi.FileID(update.Message.Document.FileID))
+					answerFileIds = append(answerFileIds, update.Message.Document.FileID)
+					msg.ReplyMarkup = doneButton
+					bot.Send(msg)
+				}
+
+				if update.Message.Text == "Готово👌" {
+					msg := tgbotapi.NewMessage(OwnerAcc, "Заявка принята")
+					msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+					bot.Send(msg)
+					log.Println("Готово👌")
+					// Готово👌
+					continue
 				}
 				/**  @todo если это документ, нужно проверять update.Message.Document.MimeType,
 				скорее всего нужно будет найти подстроку image в этом поле, если она есть, то обработать фото и переслать
 				*/
-
-				//handleOwnerMessage(update)
-				if update.Message.ReplyToMessage != nil {
-					var replyUserRequest Request
-					replyUserRequest, err = getUserRequestForMessageId(*db, update.Message.ReplyToMessage.MessageID)
-					if err != nil {
-						log.Fatal(err.Error())
-					}
-					//replyUser := db.Where("message_id = ?", update.Message.ReplyToMessage.MessageID).First(&userRequest)
-					msg := tgbotapi.NewMessage(replyUserRequest.ChatId, update.Message.Text)
-					bot.Send(msg)
-					continue
-				}
-				ownerGreeting := "Hello My Kid!"
-				msg := tgbotapi.NewMessage(OwnerAcc, ownerGreeting)
-				//msg.ReplyToMessageID = update.Message.MessageID
-
-				bot.Send(msg)
+				//msg := tgbotapi.NewMessage(OwnerAcc, update.Message.Text)
+				//bot.Send(msg)
 				continue
+				//handleOwnerMessage(update)
+				//if update.Message.ReplyToMessage != nil {
+				//	var replyUserRequest Request
+				//	replyUserRequest, err = getUserRequestForMessageId(*db, update.Message.ReplyToMessage.MessageID)
+				//	if err != nil {
+				//		log.Fatal(err.Error())
+				//	}
+				//	//replyUser := db.Where("message_id = ?", update.Message.ReplyToMessage.MessageID).First(&userRequest)
+				//	msg := tgbotapi.NewMessage(replyUserRequest.ChatId, update.Message.Text)
+				//	bot.Send(msg)
+				//	continue
+				//}
+				//ownerGreeting := "Hello My Kid!"
+				//msg := tgbotapi.NewMessage(OwnerAcc, ownerGreeting)
+				//msg.ReplyToMessageID = update.Message.MessageID
+				//
+				//bot.Send(msg)
+				//continue
 			}
 
 			// Проверка пользователя на существование
@@ -151,7 +198,6 @@ func main() {
 				}
 
 				userRequest.Step = userRequest.Step + 1
-				//db.Model(&Request{}).Where("chat_id = ?", userRequest.ChatId).Update("step", userRequest.Step+1)
 				db.Save(&userRequest)
 				continue
 			} else if userRequest.Step == 1 {
@@ -174,76 +220,128 @@ func main() {
 				continue
 			} else if userRequest.Step == 3 {
 				answer3 = update.Message.Text
-				msg.Text = "Спасибо, твоя заявка отправлена администратору!"
+				msg.Text = "Пришли фото автомобиля"
+				msg.ReplyMarkup = doneButton
 				if _, err := bot.Send(msg); err != nil {
 					log.Panic(err)
 				}
-				userRequest.Step = 4
-				userRequest.Status = StatusWaiting
+				userRequest.Step = userRequest.Step + 1
 				db.Save(&userRequest)
-
-				totalAnswer := answer1 + " \n"
-				totalAnswer += answer2 + " \n"
-				totalAnswer += answer3 + " \n"
-				totalAnswer += "ChatID: " + strconv.FormatInt(chatID, 10) + " \n"
-				msg := tgbotapi.NewMessage(OwnerAcc, totalAnswer)
-				//msg.ReplyToMessageID = update.Message.MessageID
-
-				bot.Send(msg)
 				continue
-			}
-
-			continue
-			// Проверка команда ли это?
-			if update.Message.IsCommand() {
-
-				msg := tgbotapi.NewMessage(chatID, "")
-
-				switch update.Message.Command() {
-				case "start":
-
-				default:
-					msg.Text = "Неизвестная команда"
-					if _, err := bot.Send(msg); err != nil {
-						log.Panic(err)
-					}
+			} else if userRequest.Step == 4 {
+				if update.Message.Photo != nil {
+					answerFileIds = append(answerFileIds, update.Message.Photo[1].FileID)
+					isPhotoFiles = true
+					isDocumentFiles = false
+					continue
+				} else if update.Message.Document != nil &&
+					strings.Contains(update.Message.Document.MimeType, "image") {
+					answerFileIds = append(answerFileIds, update.Message.Document.FileID)
+					isDocumentFiles = true
+					isPhotoFiles = false
 					continue
 				}
 
-				// На этом этапе мы уже обработали пользователя, получили его данные или создали новую запись
-				// todo возможно нужно вывести в лог уведомление что пользователь обработан
-				log.Println("Обработана команда /start!")
-				log.Println("Пользователь обработан!")
-			} else {
-				// Сообщение отправленное пользователем, обрабатываем и определяем на каком шаге пользователь
-				msg := tgbotapi.NewMessage(chatID, "")
-				result := db.Where("chat_id = ?", chatID).First(&userRequest)
-				if result.RowsAffected > 0 { // Есть ли пользователь в БД?
-					// Если есть пользователь, проверяем его статус
-					switch userRequest.Status {
-					case StatusAccepted:
-						// Пользователь уже зарегистрирован и добавлен в группу
-						msg.Text = "Вы уже приняты!"
-						if _, err := bot.Send(msg); err != nil {
-							log.Panic(err)
-						}
-						continue
-					case StatusDeclined:
-						// Пользователь отклонён
-						msg.Text = "Ваша заявка была отклонена!"
-						if _, err := bot.Send(msg); err != nil {
-							log.Panic(err)
-						}
-						continue
+				if update.Message.Text == "Готово👌" {
+					msg.Text = "Спасибо, твоя заявка отправлена администратору!"
+					msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+					if _, err := bot.Send(msg); err != nil {
+						log.Panic(err)
 					}
 
-				} else {
-					// Если запись не найдена, создаем нового пользователя
-					userRequest = Request{ChatId: chatID}
-					db.Create(&userRequest)
-					// todo Возможно проверить на ошибку создания пользователя?
+					// Обновляем статусы
+					userRequest.Step = 5
+					userRequest.Status = StatusWaiting
+					db.Save(&userRequest)
+					// Готово👌
+
+					// Отправка заявки админу
+					totalAnswer := answer1 + " \n"
+					totalAnswer += answer2 + " \n"
+					totalAnswer += answer3 + " \n"
+					totalAnswer += "ChatID: " + strconv.FormatInt(chatID, 10) + " \n"
+					// Отправляем текст заявки
+					msg := tgbotapi.NewMessage(OwnerAcc, totalAnswer)
+					if _, err := bot.Send(msg); err != nil {
+						log.Panic(err)
+					}
+
+					files := make([]interface{}, len(answerFileIds))
+					for i, s := range answerFileIds {
+						if isPhotoFiles {
+							files[i] = tgbotapi.NewInputMediaPhoto(tgbotapi.FileID(s))
+							continue
+						}
+
+						if isDocumentFiles {
+							files[i] = tgbotapi.NewInputMediaDocument(tgbotapi.FileID(s))
+							continue
+						}
+					}
+					cfg := tgbotapi.NewMediaGroup(
+						OwnerAcc,
+						files,
+					)
+
+					bot.SendMediaGroup(cfg)
+
+					// todo придумать как чистить массив с файлами? Если это нужно будет? Массив не очищается после заполнения файлами
+					answerFileIds = nil
+					continue
 				}
 			}
+
+			//continue
+			// Проверка команда ли это?
+			//if update.Message.IsCommand() {
+			//
+			//	msg := tgbotapi.NewMessage(chatID, "")
+			//
+			//	switch update.Message.Command() {
+			//	case "start":
+			//
+			//	default:
+			//		msg.Text = "Неизвестная команда"
+			//		if _, err := bot.Send(msg); err != nil {
+			//			log.Panic(err)
+			//		}
+			//		continue
+			//	}
+			//
+			//	// На этом этапе мы уже обработали пользователя, получили его данные или создали новую запись
+			//	// todo возможно нужно вывести в лог уведомление что пользователь обработан
+			//	log.Println("Обработана команда /start!")
+			//	log.Println("Пользователь обработан!")
+			//} else {
+			//	// Сообщение отправленное пользователем, обрабатываем и определяем на каком шаге пользователь
+			//	msg := tgbotapi.NewMessage(chatID, "")
+			//	result := db.Where("chat_id = ?", chatID).First(&userRequest)
+			//	if result.RowsAffected > 0 { // Есть ли пользователь в БД?
+			//		// Если есть пользователь, проверяем его статус
+			//		switch userRequest.Status {
+			//		case StatusAccepted:
+			//			// Пользователь уже зарегистрирован и добавлен в группу
+			//			msg.Text = "Вы уже приняты!"
+			//			if _, err := bot.Send(msg); err != nil {
+			//				log.Panic(err)
+			//			}
+			//			continue
+			//		case StatusDeclined:
+			//			// Пользователь отклонён
+			//			msg.Text = "Ваша заявка была отклонена!"
+			//			if _, err := bot.Send(msg); err != nil {
+			//				log.Panic(err)
+			//			}
+			//			continue
+			//		}
+			//
+			//	} else {
+			//		// Если запись не найдена, создаем нового пользователя
+			//		userRequest = Request{ChatId: chatID}
+			//		db.Create(&userRequest)
+			//		// todo Возможно проверить на ошибку создания пользователя?
+			//	}
+			//}
 
 			// найти пользователя, либо создать его
 			//db.Clauses(clause.OnConflict{
@@ -277,68 +375,31 @@ func main() {
 
 			// Если это просто сообщение
 
-			if update.Message.From.ID == OwnerAcc {
-				//handleOwnerMessage(update)
-				if update.Message.ReplyToMessage != nil {
-					var replyUserRequest Request
-					replyUserRequest, err = getUserRequestForMessageId(*db, update.Message.ReplyToMessage.MessageID)
-					if err != nil {
-						log.Fatal(err.Error())
-					}
-					//replyUser := db.Where("message_id = ?", update.Message.ReplyToMessage.MessageID).First(&userRequest)
-					msg := tgbotapi.NewMessage(replyUserRequest.ChatId, update.Message.Text)
-					bot.Send(msg)
-					continue
-				}
-				ownerGreeting := "Hello My Kid!"
-				msg := tgbotapi.NewMessage(OwnerAcc, ownerGreeting)
-				//msg.ReplyToMessageID = update.Message.MessageID
-
-				bot.Send(msg)
-				continue
-			}
+			//if update.Message.From.ID == OwnerAcc {
+			//	//handleOwnerMessage(update)
+			//	if update.Message.ReplyToMessage != nil {
+			//		var replyUserRequest Request
+			//		replyUserRequest, err = getUserRequestForMessageId(*db, update.Message.ReplyToMessage.MessageID)
+			//		if err != nil {
+			//			log.Fatal(err.Error())
+			//		}
+			//		//replyUser := db.Where("message_id = ?", update.Message.ReplyToMessage.MessageID).First(&userRequest)
+			//		msg := tgbotapi.NewMessage(replyUserRequest.ChatId, update.Message.Text)
+			//		bot.Send(msg)
+			//		continue
+			//	}
+			//	ownerGreeting := "Hello My Kid!"
+			//	msg := tgbotapi.NewMessage(OwnerAcc, ownerGreeting)
+			//	//msg.ReplyToMessageID = update.Message.MessageID
+			//
+			//	bot.Send(msg)
+			//	continue
+			//}
 
 			//msg = tgbotapi.NewForward(OwnerAcc, update.Message.From.ID, update.Message.MessageID)
 			//msg.ReplyToMessageID = update.Message.MessageID
 
-			bot.Send(msg)
+			//bot.Send(msg)
 		}
 	}
 }
-
-//func handleOwnerMessage(update tgbotapi.Update) {
-//}
-//
-//func handleCommand(update tgbotapi.Update) {
-//}
-
-//func openDb() *badger.DB {
-//	db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	defer db.Close()
-//
-//	return db
-//}
-
-func getUserRequestForMessageId(db gorm.DB, messageId int) (Request, error) {
-	var userRequest Request
-
-	res := db.Where("message_id = ?", messageId-1).First(&userRequest)
-	if res.Error != nil {
-		log.Fatal(res.Error.Error())
-		return userRequest, res.Error
-	}
-
-	return userRequest, nil
-}
-
-//func findUser(chatID int64) bool {
-//	find, err := db.Where("chat_id = ?", "chatID").First(&user)
-//	if err != nil {
-//		msg := tgbotapi.NewMessage(chatID, "Произошла ошибка! Бот может работать неправильно!")
-//		telegramBot.API.Send(msg)
-//	}
-//	return find
-//}
