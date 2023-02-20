d560118bec5a6a67ac5a47c341d8819b71377ae1
artem-brezutsky <artem-brezutsky>
2023-02-21 00:04:55 +0200

    tgb - refactoring

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -12,30 +12,34 @@ import (
 )
 
 func main() {
+	// Подключаем файл .env
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("No .env file found")
 	}
 
+	// Подключаемся к базе данным
 	db, err := gorm.Open(mysql.Open(os.Getenv("DSN")), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
+	// Выполняем миграции
 	err = db.AutoMigrate(&models.User{})
 	if err != nil {
 		log.Fatal("Не удалось выполнить миграцию: ", err)
 	}
 
+	// Инициализируем Telegram Bot API
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
 	if err != nil {
 		log.Fatal("Не удалось инициализировать Telegram Bot API: ", err)
 	}
 
-	bot.Debug = true
+	//bot.Debug = true
 
+	// Создаём новый экземпляр бота
 	telegramBot := telegram.NewBot(bot, db)
 	if err := telegramBot.Start(); err != nil {
 		log.Fatal(err)
 	}
-
 }
